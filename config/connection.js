162cf73c9004e716cba9a1f1bb3a43391f719107
Kenlau94 <kenlauwork94@gmail.com>
2023-11-05 04:37:28 -0500

    half way done

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 
 //?????????????????
+mongoose.connect(process.env.MONOGDB_URI || "mongodb://localhost:3001", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = mongoose.connection;
--- models/User.js
+++ models/User.js
@@ -17,7 +17,7 @@ const userSchema = new Schema(
         validator: function (v) {
           return /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(
             v
-          );
+          ); //regexxxxxxx
         },
       },
     },
