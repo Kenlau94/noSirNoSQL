0a97732351d2fd2981baff789cdee5fd827bec70
Kenlau94 <kenlauwork94@gmail.com>
2023-11-12 17:23:49 -0500

    added imports, port

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
-const express = require();
-const db = require();
-const routes = require();
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Middleware configurations
+//parses incoming request with urlencoded, parses it to jsom, handles via routes
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+// Connect to MongoDB b4 express
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`Server is now listening on PORT ${PORT}.`);
+  });
+});
